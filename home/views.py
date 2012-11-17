cd5a8fd2cebb3eb4f79bec0abf9f4fc4f766e7cd
Felipe Ramos <perenecabuto@gmail.com>
2012-11-17 16:57:22 -0200

    Removendo app depreciada

--- home/views.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding : utf-8 -*-
-from django.shortcuts import render_to_response
-from django.template.loader import get_template
-from django.template import RequestContext
-
-# Create your views here.
-def index(request):
-  return render_to_response('home/index.html', {})
-
